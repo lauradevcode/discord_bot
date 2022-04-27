047ad73f478b8e5a331744482fd00898db10108d
Laura Beatriz <49999046+lauradevcode@users.noreply.github.com>
2022-04-27 17:49:05 -0300

    Update função de saudação e resposta

--- primeiro_bot.py
+++ primeiro_bot.py
@@ -5,6 +5,25 @@ bot = commands.Bot("!")
 
 @bot.event
 async def on_ready():
-    print(f"Olá, estou pronto para iniciar! Estou conectado como {bot.user} #Bot Misaki Mei")
+    print(f"Olá, estou pronto para iniciar! Estou conectado como {bot.user}")
 
-bot.run("OTY4OTMwNzMwMzAwMjc2Nzc3.YmmBDw.-0kGrl0MhqjvFvqLbhIKqErpUlY")
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if "palavrão" in message.content:
+        await message.channel.send(f"Por favor, {message.author.name}, não ofenda os demais usuários")
+
+        await message.delete()
+
+    await bot.process_commands(message)
+
+@bot.command( name="oi" )
+async def send_hello(ctx):
+    name = ctx.author.name
+
+    response = "Olá, "+ name
+    await ctx.send(response)
+
+bot.run(f"OTY4OTMwNzMwMzAwMjc2Nzc3.YmmBDw.CT9LscWBLaAvkr0OwFY-DJLxYAc")
